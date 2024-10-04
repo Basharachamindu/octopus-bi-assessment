1d1598e272a86262ea122c04dd75d683eaa6cb29
Basharachamindu <124559011+Basharachamindu@users.noreply.github.com>
2024-10-04 15:48:32 +0530

    Update urls.py

--- interview/urls.py
+++ interview/urls.py
@@ -21,5 +21,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('interview_app.urls')),  # Include your app URLs at the root
+    path('', include('interview_app.urls')),  
 ]
